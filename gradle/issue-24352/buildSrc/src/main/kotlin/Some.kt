1d42462c51aa4ca120d2da67853aac0f7fb31044
Paul Merlin <paul@gradle.com>
2023-03-16 13:58:17 +0100

    Add mcve for gradle/gradle#24352

---
+++ gradle/issue-24352/buildSrc/src/main/kotlin/Some.kt
@@ -0,0 +1,5 @@
+// run and then comment `internal` and then run again with info logging enabled
+// it shows `Skipping Kotlin DSL script compilation (Project/TopLevel/stage2) as it is up-to-date.`
+// it should recompiled instead
+internal
+interface Some
