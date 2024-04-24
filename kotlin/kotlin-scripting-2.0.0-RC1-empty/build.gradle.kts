96c12646e986d8a2dd74e7e3b3adbd724fac5576
Paul Merlin <paul@gradle.com>
2024-04-24 13:54:43 +0200

    Kotlin 2.0.0-RC1 K2 Scripting with empty scripts

---
+++ kotlin/kotlin-scripting-2.0.0-RC1-empty/build.gradle.kts
@@ -0,0 +1,16 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "2.0.0-RC1"
+    `kotlin-dsl`
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        apiVersion = KotlinVersion.KOTLIN_2_0
+        languageVersion = KotlinVersion.KOTLIN_2_0
+        // w: Scripts are not yet supported with K2 in LightTree mode, consider using K1 or disable LightTree mode with -Xuse-fir-lt=false
+        freeCompilerArgs.add("-Xuse-fir-lt=false")
+    }
+}
---
+++ kotlin/kotlin-scripting-2.0.0-RC1-empty/settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kotlin-scripting-2.0.0-RC1-empty"
